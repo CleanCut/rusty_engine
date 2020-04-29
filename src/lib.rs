ced2290f72b738bee9e269559c635e96135bb1fa
Nathan Stocks <cleancut@github.com>
2020-04-29 00:13:45 -0600

    Add glm and Vec2 to prelude. Update rusty_core and rusty_gfx imports. Bump to 0.5.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 pub use rusty_audio as audio;
-pub use rusty_gfx as gfx;
 pub use rusty_core as core;
+pub use rusty_gfx as gfx;
 
 // rusty_core's re-exports should all be attached to the root of rusty_engine
-pub use rusty_core::glm as glm;
+pub use rusty_core::glm;
 
 pub mod prelude {
+    pub use glm::{self, Vec2};
     pub use rusty_audio::prelude::*;
-    pub use rusty_gfx::prelude::*;
     pub use rusty_core::prelude::*;
+    pub use rusty_gfx::prelude::*;
 }
 
 #[cfg(test)]
