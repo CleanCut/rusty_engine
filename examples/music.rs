60a0ad780f2bb6c4b1671d7da69ccbec7546e532
Nathan Stocks <cleancut@github.com>
2021-11-12 16:08:04 -0700

    refactor music app to rotate through music whenever a key is pressed

--- examples/music.rs
+++ examples/music.rs
@@ -2,41 +2,45 @@ use rusty_engine::prelude::*;
 
 fn main() {
     let mut game = Game::new();
-    game.game_state_mut()
-        .timer_map
-        .insert("music change timer".into(), Timer::from_seconds(30.0, true));
-    let _ = game.add_text_actor(
+    let msg = game.add_text_actor(
         "msg",
-        "If you are not running with \"--release\", it may take several seconds for each song to load.",
+        "Press any key to advance to the next music selection.\n\nIf you are not running with \"--release\", it may take several seconds for each song to load!",
     );
+    msg.translation.y = -200.0;
 
     game.run(logic);
 }
 
 fn logic(game_state: &mut GameState) {
-    // gain another life every time the timer goes off
-    if game_state.bool_map.get("music started").is_none() {
-        let note1 =
-            game_state.add_text_actor("note1", "Playing MysteriousMagic for about 30 seconds.");
-        note1.translation.y = -100.0;
-        game_state.bool_map.insert("music started".into(), true);
-        game_state
-            .audio_manager
-            .play_music(MusicPreset::MysteriousMagic, 1.0);
-    }
-    if game_state
-        .timer_map
-        .get_mut("music change timer")
-        .unwrap()
-        .tick(game_state.delta)
-        .just_finished()
-    {
-        if game_state.bool_map.get("music changed").is_none() {
-            println!("Switching to Classy8Bit...forever. Press Esc on the GUI Window or Ctrl-C in the terminal to quit.");
-            game_state.bool_map.insert("music changed".into(), true);
-            game_state
-                .audio_manager
-                .play_music(MusicPreset::Classy8Bit, 1.0);
+    let mut should_play_new_song = false;
+
+    // Play a "new" song if nothing has been played before
+    let current_music_index = game_state
+        .usize_map
+        .entry("current music index".into())
+        .or_insert_with(|| {
+            should_play_new_song = true;
+            0
+        });
+
+    // Play a new song because a key was pressed
+    for ev in game_state.keyboard_events.drain(..) {
+        if ev.state != ElementState::Pressed {
+            continue;
         }
+        *current_music_index = (*current_music_index + 1) % MusicPreset::variant_iter().count();
+        should_play_new_song = true;
+        break;
+    }
+
+    if should_play_new_song {
+        // Actually play the new song
+        let music_preset = MusicPreset::variant_iter()
+            .nth(*current_music_index)
+            .unwrap();
+        game_state.audio_manager.play_music(music_preset, 1.0);
+        // And make a text actor saying what song we're playing
+        let note1 = game_state.add_text_actor("note1", format!("Looping: {:?}", music_preset));
+        note1.font_size = 75.0;
     }
 }
